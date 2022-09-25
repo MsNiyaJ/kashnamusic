67cb75d0cf7576036ff5f2c7932545f380291e8c
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-09-25 00:31:41 -0400

    removed closing tag of iframe

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -12,7 +12,7 @@ const Video = ({ src, title }: IVideo) => {
         src={src}
         title={title}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-      ></iframe>
+      />
       <h2>{title}</h2>
     </div>
   );
