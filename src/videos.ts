f805a02ce4244a662124febf1e46847a18ff1903
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-09-25 00:58:42 -0400

    fixed video link

--- src/videos.ts
+++ src/videos.ts
@@ -1,38 +1,38 @@
 export const videos = [
   {
     id: 1,
-    src: 'https://www.youtube.com/embed/KGBLYJeR1G8',
+    src: 'https://www.youtube.com/embed/KGBLYJeR1G8?&muted=1',
     title: 'The Beginning (Intro)',
   },
   {
     id: 2,
-    src: 'https://www.youtube.com/embed/gfDxSENsuhA',
+    src: 'https://www.youtube.com/embed/gfDxSENsuhA?&muted=1',
     title: 'Progress',
   },
   {
     id: 3,
-    src: 'https://www.youtube.com/embed/T1jcU00gxOc',
+    src: 'https://www.youtube.com/embed/T1jcU00gxOc?&muted=1',
     title: 'Moments',
   },
   {
     id: 4,
-    src: 'https://www.youtube.com/embed/8ok2PHHrZGU',
+    src: 'https://www.youtube.com/embed/8ok2PHHrZGU?&muted=1',
     title: 'Dreams',
   },
   {
     id: 5,
-    src: 'https://www.youtube.com/embed/h1eZDo8cATw',
+    src: 'https://www.youtube.com/embed/h1eZDo8cATw?&muted=1',
     title: 'Kashna Speed Off (Official Visualizer)',
   },
 
   {
     id: 6,
-    src: 'https://www.youtube.com/embed/BtpaGUDcswY',
+    src: 'https://www.youtube.com/embed/BtpaGUDcswY?&muted=1',
     title: 'Letter To Kason',
   },
   {
     id: 7,
-    src: 'https://www.youtube.com/embed/PmJqnKXqRmg',
+    src: 'https://www.youtube.com/embed/PmJqnKXqRmg?&muted=1',
     title: 'Ocean',
   },
 ];
